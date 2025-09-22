829129878e8d7d8ce0d514486d36084616c61180
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-09-22 19:47:47 +0300

    feat(api): add POST /api/transactions endpoint with validation and DB insert

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { sql } from './config/db.js';
 dotenv.config();
 
 const app = express();
+//Middleware to get this in post req  const { user_id, title, amount, category } = req.body;
+app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
@@ -25,8 +27,26 @@ const initDB = async () => {
   }
 };
 
-app.get('/', (req, res) => {
-  res.send('It is working!');
+app.post('/api/transactions', async (req, res) => {
+  try {
+    const { user_id, title, amount, category } = req.body;
+
+    if (!title || !user_id || !category || amount === undefined) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const transaction = await sql`
+    INSERT INTO transactions(user_id, title, amount, category)
+    VALUES (${user_id},${title},${amount},${category})
+    RETURNING *
+`;
+    console.log(transaction);
+
+    res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.log('Error creating the transaction', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 await initDB();
