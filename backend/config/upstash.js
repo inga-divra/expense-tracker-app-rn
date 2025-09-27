77cb6028ebf84a2b3dcc7fe8369b09b803f2af41
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-09-27 22:05:12 +0300

    Added config/upstash.js for Redis connection setup with @upstash/redis

---
+++ backend/config/upstash.js
@@ -0,0 +1,8 @@
+import { Redis } from '@upstash/redis';
+const redis = new Redis({
+  redis: Redis.fromEnv(),
+  limiter: RateLimit(),
+});
+
+await redis.set('foo', 'bar');
+await redis.get('foo');
