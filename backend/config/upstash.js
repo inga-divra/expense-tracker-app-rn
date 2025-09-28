ac09dbe1969dcc78900b2bbfdca62493f616872e
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-09-28 20:43:45 +0300

    chore: install @upstash/ratelimit and @upstash/redis for implementing rate limiting and caching.

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -1,8 +1,6 @@
 import { Redis } from '@upstash/redis';
+import { RateLimit } from '@upstash/ratelimit';
 const redis = new Redis({
   redis: Redis.fromEnv(),
   limiter: RateLimit(),
 });
-
-await redis.set('foo', 'bar');
-await redis.get('foo');
