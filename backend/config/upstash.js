14521079fb37cf6cd4c7ab2534645af0b989ed58
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-09-30 16:09:49 +0300

    feat: configure Upstash Redis and rate limiter for API requests.

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -1,6 +1,10 @@
 import { Redis } from '@upstash/redis';
-import { RateLimit } from '@upstash/ratelimit';
-const redis = new Redis({
+import { Ratelimit } from '@upstash/ratelimit';
+import 'dotenv/config';
+
+const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: RateLimit(),
+  limiter: Ratelimit.slidingWindow(100, '60 s'),
 });
+
+export default ratelimit;
