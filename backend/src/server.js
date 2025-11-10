cd6cd498be994944151601b98147ca2d8eb2f61a
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-11-10 19:43:16 +0200

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,23 +2,32 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
-
 import transactionsRoute from './routes/transactionsRoute.js';
 
 dotenv.config();
 
 const app = express();
 
-//Middleware to get this in post req  const { user_id, title, amount, category } = req.body;
+// Middleware
 app.use(rateLimiter);
 app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
+// Routes
+app.use('/api/transactions', transactionsRoute);
 
-app.use('/api/transactions/', transactionsRoute);
+// Health-check endpoint (Render)
+app.get('/api/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+  });
+});
 
+// Init DB
 await initDB();
 
+// Start server
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is up and running on PORT: ${PORT}`);
 });
