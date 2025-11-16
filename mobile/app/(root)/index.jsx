e9fee1e9b21a165c8832456021ee656245b87fe3
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-11-16 17:51:17 +0200

    feat(ui): add PageLoader component and show it during home page loading.

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native'
 import { SignOutButton } from '@/components/SignOutButton'
 import { useTransactions } from '../../hooks/useTransactions'
 import { useEffect } from 'react'
+import PageLoader from '../../components/PageLoader'
 export default function Page() {
     const { user } = useUser()
     const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(user.id)
@@ -11,11 +12,11 @@ export default function Page() {
     useEffect(() => {
         loadData()
     }, [loadData])
-    console.log('userId', user.id);
 
-    console.log('transactions', transactions);
-    console.log('summary', summary);
 
+    if (isLoading) {
+        return <PageLoader />
+    }
 
     return (
         <View>
